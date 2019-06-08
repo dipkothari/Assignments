2c62d3434a00bc3e4818275a0ffa34ea9da0a73d
Deep <Deep@192.168.1.83>
2019-06-08 16:44:30 -0500

    Commit

---
+++ Day2/src/Assignment1/Assignment1.java
@@ -0,0 +1,11 @@
+package Assignment1;
+
+public class Assignment1 {
+	public static void main(String args[]) {
+        byte byteA = 127;
+        byte byteB = -128;
+       byte ans = (byte)(byteA + byteB);
+       System.out.println();
+       System.out.println(ans);
+    }
+}

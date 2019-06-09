7653da0f1ca59715de70a9eebfd581189ad3330e
Deep <Deep@192.168.1.83>
2019-06-08 22:32:56 -0500

    Commit

---
+++ Day2/src/SelfReview/Example1.java
@@ -0,0 +1,9 @@
+package SelfReview;
+
+public class Example1 {
+	public static void main(String[] args) {
+		int intValOne=5, intValTwo=7;
+		System.out.println(((intValTwo * 2)% intValOne));
+		System.out.println((intValTwo % intValOne));
+	}
+}

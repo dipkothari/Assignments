60af22533c9cbb290ff63c5f706e017dafb1b1ca
Deep <Deep@192.168.1.83>
2019-06-08 20:38:55 -0500

    commit

---
+++ Day2/src/Assignment9/Student.java
@@ -0,0 +1,23 @@
+package Assignment9;
+
+public class Student {
+	private static int studentId = 550;
+	private char studentType;
+	private String studentName;
+	Student() {
+		//this.studentId=550;
+		studentId++;
+	}
+	Student(char sType, String fname, String lname){
+		this();
+		studentType = sType;
+		studentName=fname + lname;
+	}
+	public void displayDetails(Student obj) {
+		System.out.println("studentID :"+studentId);
+		System.out.println("studentType :"+obj.studentType);
+		System.out.println("studentName :"+obj.studentName);
+		
+	}
+
+}
---
+++ Day2/src/Assignment9/display.java
@@ -0,0 +1,13 @@
+package Assignment9;
+
+public class display {
+
+	public static void main(String[] args) {
+		Student obj1 = new Student('D',"Bony","Thomas");
+		obj1.displayDetails(obj1);
+		Student obj2 = new Student('H',"Dinil","Bose");
+		obj2.displayDetails(obj2);
+
+	}
+
+}

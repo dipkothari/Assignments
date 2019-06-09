ce1759bf238d0a84e62425744951d36e5163dfc1
Deep <Deep@192.168.1.83>
2019-06-08 21:55:54 -0500

    commit

---
+++ Day2/src/SelfReview/Demo.java
@@ -0,0 +1,14 @@
+package SelfReview;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		//boolean a= true;
+		if(false) {
+			System.out.println("True Block");
+		}
+		else {
+			System.out.println("False BLock");
+		}
+	}
+}
---
+++ Day2/src/SelfReview/TypeCasting.java
@@ -0,0 +1,11 @@
+package SelfReview;
+
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		int intVal=10;
+		double doubleVal = 20.0;
+		intVal = (int) doubleVal;
+		System.out.println(intVal);
+	}
+}
